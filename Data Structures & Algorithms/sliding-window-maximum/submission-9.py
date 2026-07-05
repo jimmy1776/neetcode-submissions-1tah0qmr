5c0ad42cdd3a5f293deee12e15635ddad74f0440
J. Cedeno <93452981+jimmy1776@users.noreply.github.com>
2026-07-04 21:41:32 -0400

    Add: sliding-window-maximum - submission-9

---
+++ Data Structures & Algorithms/sliding-window-maximum/submission-9.py
@@ -0,0 +1,38 @@
+class Solution:
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        output = []
+        q = collections.deque()
+        l = r = 0 
+
+        while r < len(nums):
+            #Pop the smaller values from the deck
+            while q and nums[q[-1]] < nums[r]:
+                q.pop()
+            q.append(r)
+
+            #remove left val from window 
+            if l > q[0]:
+                q.popleft()
+
+            if(r + 1) >= k:
+                l += 1 
+                output.append(nums[q[0]])
+            
+            r += 1 
+        return output
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#q[0] is the leftmostelement (the front) and q[-1] is the rightmost element (the back/tail)
