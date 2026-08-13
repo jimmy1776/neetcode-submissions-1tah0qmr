20b382385f5e581b8d6f05c769f922b412b46067
J. Cedeno <93452981+jimmy1776@users.noreply.github.com>
2026-08-12 22:59:06 -0400

    Add: subsets - submission-16

---
+++ Data Structures & Algorithms/subsets/submission-16.py
@@ -0,0 +1,22 @@
+class Solution: 
+    def subsets(self,nums: List[int]) -> List[List[int]]:
+        res = []
+        cur = []
+
+        def dfs(i): 
+            #base case 
+            if i >= len(nums):
+                res.append(cur.copy())
+                return  
+
+            #recursive case 
+            cur.append(nums[i])
+            dfs(i+1)
+            cur.pop()
+            dfs(i+1)
+        dfs(0)
+        return res 
+
+
+
+        
