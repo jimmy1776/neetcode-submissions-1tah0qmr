0bd9e8c6418f06a5391b051b7167323e3252c70b
J. Cedeno <93452981+jimmy1776@users.noreply.github.com>
2026-08-12 23:12:41 -0400

    Add: combination-target-sum - submission-11

---
+++ Data Structures & Algorithms/combination-target-sum/submission-11.py
@@ -0,0 +1,63 @@
+class Solution:
+    def combinationSum(self, nums: List[int], target: int) -> List[List[int]]:
+        subset = []
+        res = []
+
+        def dfs(i,cur,total):
+            #base case 
+            if total == target:
+                res.append(cur.copy())
+                return 
+            if total > target or i >= len(nums):
+                return 
+            
+            #recursive case 
+            cur.append(nums[i])
+            dfs(i,cur,total + nums[i])
+            #backtrack 
+            cur.pop()
+            dfs(i + 1, cur, total)
+        dfs(0,[],0)
+        return res 
+
+
+
+    
+
+
+
+
+    #i = index, cur = [] current subarray, total : running total of the cur subarray  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
