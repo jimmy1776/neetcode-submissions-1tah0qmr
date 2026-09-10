3e7bdbc3a2e7f5bec61fd1486ca976261d702ed0
J. Cedeno <93452981+jimmy1776@users.noreply.github.com>
2026-09-10 12:39:23 -0400

    Add: course-schedule - submission-9

---
+++ Data Structures & Algorithms/course-schedule/submission-9.py
@@ -0,0 +1,29 @@
+class Solution:
+   def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool: 
+    #map each course to preqList 
+    #for every course intially I wanna map it to an empty list 
+    preMap = {i: [] for i in range(numCourses)}
+    for crs, pre  in prerequisites:
+        preMap[crs].append(pre)
+    
+    #visitSet = all courses along the curr DFS path 
+    visitSet = set()
+    def dfs(crs):
+        #base cases
+        if crs in visitSet:
+            return False 
+        if preMap[crs]==[]:
+            return True 
+        
+        #recurisve case 
+        visitSet.add(crs)
+        for pre in preMap[crs]:
+            if not dfs(pre): return False 
+        visitSet.remove(crs)
+        preMap[crs] = []
+        return True 
+    
+    for crs in range(numCourses):
+        if not dfs(crs):return False 
+    return True 
+
