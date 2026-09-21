603798d873127def128a59ad550d103c7cbf1287
J. Cedeno <93452981+jimmy1776@users.noreply.github.com>
2026-09-21 11:46:14 -0400

    Add: reconstruct-flight-path - submission-6

---
+++ Data Structures & Algorithms/reconstruct-flight-path/submission-6.py
@@ -0,0 +1,17 @@
+class Solution:
+    def findItinerary(self, tickets: List[List[str]]) -> List[str]:
+        adj = defaultdict(list)
+        for src, dst in sorted(tickets):
+            adj[src].append(dst)
+        
+        res = []
+
+        def dfs(src):
+            while adj[src]:
+                nxt = adj[src].pop(0) #take smallest remaining destination
+                dfs(nxt)
+            res.append(src)
+        
+        dfs("JFK")
+        #return a reversed copy of the entire list 
+        return res [::-1]
